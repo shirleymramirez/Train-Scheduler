05a7980a523fa80877adab22ef2ccf087d9d63ac
jheilhet <shirley.mramirez@yahoo.com>
2017-12-05 12:56:22 -0700

    html page layout

---
+++ assets/javascript/logic.js
@@ -0,0 +1,16 @@
+var config = {
+    apiKey: "AIzaSyB9gNk63qxHlXhsnRWyw6ZoBlkT_DER4Bc",
+    authDomain: "trainscheduler-a4ab4.firebaseapp.com",
+    databaseURL: "https://trainscheduler-a4ab4.firebaseio.com",
+    projectId: "trainscheduler-a4ab4",
+    storageBucket: "",
+    messagingSenderId: "1039027669333"
+};
+firebase.initializeApp(config);
+
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = "";
+
+$("#")
